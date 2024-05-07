5039e82bc680d28d717da625c293a2e7bbce1443
Ismail <hummetzadeismail@gmail.com>
2024-05-07 05:16:12 +0300

    commit

--- src/components/swiper/Swiper.tsx
+++ src/components/swiper/Swiper.tsx
@@ -47,7 +47,7 @@ export const SwiperComponent = ({ items }: { items: Item[] }) => {
         modules={[Autoplay, Pagination, Navigation]}
         centeredSlides={true}
         flipEffect={{ slideShadows: true }}
-        // autoplay={{ delay: 1500, disableOnInteraction: false }}
+        autoplay={{ delay: 1500, disableOnInteraction: false }}
         className='mySwiper'
       >
         {items.map(({}, index) => {
